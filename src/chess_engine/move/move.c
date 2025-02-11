3371e4717bd417c63ae004bfca0bafd3409472f9
Carto1a <49589600+Carto1a@users.noreply.github.com>
2025-02-11 16:12:21 -0300

    feat(chess_engine): basic code for move

--- src/chess_engine/move/move.c
+++ src/chess_engine/move/move.c
@@ -1,14 +1,30 @@
 #include "move.h"
+#include "../chess.h"
 #include <stdlib.h>
 
 chess_move *chess_move_initalize(chess_move_type type, chess_piece *piece,
                                  int x, int y) {
   chess_move *move = malloc(sizeof(chess_move));
+  if (!move) {
+    chess_set_error("Memory allocation failed");
+    return NULL;
+  }
 
-  move->y = y;
   move->x = x;
+  move->y = y;
   move->piece = piece;
   move->type = type;
 
   return move;
+}
+
+int chess_move_dispose(chess_move *move) {
+  if (!move) {
+    chess_set_error("Attempt to deallocate a NULL move");
+    return CHESS_ERROR;
+  }
+
+  free(move);
+
+  return CHESS_SUCESS;
 }
