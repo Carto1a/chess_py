c059dce5f6733235d44240264d6346413b93a0bf
Cartola <49589600+Carto1a@users.noreply.github.com>
2025-02-14 16:20:32 -0300

    agora sim

--- common/src/unix.c
+++ common/src/unix.c
@@ -1,15 +1,15 @@
 #include "common.h"
-#include <sys/types.h>
+#include <stddef.h>
+#define _POSIX_C_SOURCE 199309L
 #include <time.h>
+#include <sys/types.h>
+#include <string.h>
 
 int sleep(unsigned int milliseconds) {
   struct timespec spec;
 
-  if (milliseconds <= 999) {
-    spec.tv_nsec = milliseconds * 1000000;
-  } else {
-    spec.tv_sec = milliseconds / 1000;
-  }
+  spec.tv_sec = milliseconds / 1000;
+  spec.tv_nsec = (__syscall_slong_t)((milliseconds % 1000) * 1000000);
 
   return nanosleep(&spec, NULL);
 }
