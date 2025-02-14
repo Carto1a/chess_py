6a95d2d9b69a3b4a7a06bdecabc72ea4452b6196
Carto1a <49589600+Carto1a@users.noreply.github.com>
2025-02-14 15:07:55 -0300

    odeio o windows

---
+++ main.c
@@ -0,0 +1,8 @@
+/* #include "interface.h" */
+
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+  /* interface_initialize(); */
+  printf("teste");
+  return 0;
+}
--- nop/src/main.c
+++
@@ -1,6 +0,0 @@
-#include "interface.h"
-
-int main(int argc, char *argv[]) {
-  interface_initialize();
-  return 0;
-}
